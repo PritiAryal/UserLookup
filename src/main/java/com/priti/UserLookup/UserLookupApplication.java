fc5c1e4277cd537c3936f2135c6fd6bf70c44bff
Priti Aryal <40359121+PritiAryal@users.noreply.github.com>
2025-06-09 17:32:00 -0400

    Basic Configuration - dependencies, properties

---
+++ src/main/java/com/priti/UserLookup/UserLookupApplication.java
@@ -0,0 +1,13 @@
+package com.priti.UserLookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priti/UserLookup/UserLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priti.UserLookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
